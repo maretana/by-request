822426dc3e7dbad8c76b1b47b056a5d102e8186f
Mario Retana <mario.retana@omni.cr>
2020-10-04 22:43:22 -0600

    added get songs from db

--- pages/vote.js
+++ pages/vote.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import songs from '../lib/songs'
 
 export default function VotePage (props) {
+  console.log('props', props)
   return <div />
+}
+
+export async function getStaticProps (context) {
+  const songsHash = await songs.getSongsHash()
+  return {
+    props: {
+      albums: songsHash
+    }
+  }
 }
