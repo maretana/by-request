8f7ed399e832a721757567b07a9e2c7f139db8c3
Mario Retana <mario.retana@omni.cr>
2020-11-01 22:20:04 -0600

    added open-close functionality

--- components/AlbumCard/AlbumCard.jsx
+++ components/AlbumCard/AlbumCard.jsx
@@ -5,16 +5,21 @@ import styles from './AlbumCard.module.scss'
 
 export default function AlbumCard ({ name, artwork, year, songs }) {
   const [isOpen, setIsOpen] = useState(false)
+
+  function onClickHandler () {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <li className={styles.AlbumCardListItem}>
+    <li className={styles.AlbumCardListItem} onClick={onClickHandler}>
       <section className={styles.AlbumCard}>
         <img src={artwork} alt={`${name} artwork`} />
         <div className={styles.TextContainer}>
           <h1 className={styles.AlbumTitle}>{name}</h1>
           <p className={styles.AlbumYear}>{year}</p>
-          {isOpen && (<SongsList songs={songs} />)}
         </div>
       </section>
+      {isOpen && (<SongsList songs={songs} />)}
     </li>
   )
 }
