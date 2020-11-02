2bf533676ff6f36eca655b68ba79ce79c5e07712
Mario Retana <mario.retana@omni.cr>
2020-11-01 22:06:19 -0600

    fixed head noscript tag

--- pages/_app.js
+++ pages/_app.js
@@ -6,19 +6,19 @@ import '../styles/global.scss'
 function MyApp ({ Component, pageProps }) {
   const GOOGLE_FONTS = 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&family=Mulish:ital,wght@0,400;0,700;1,400;1,700&display=swap'
   return (
-    <>
+    <Layout>
       <Head>
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
         <link rel='preload' as='style' href={GOOGLE_FONTS} />
         <link rel='stylesheet' href={GOOGLE_FONTS} media='print' onLoad="this.media='all'" />
-        <noscript>
-          <link rel='stylesheet' href={GOOGLE_FONTS} />
-        </noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<link rel="stylesheet" href="${GOOGLE_FONTS}">`
+          }}
+        />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
