ae46739badd292e4f143004b51c12c3f7e32c744
Mario Retana <mario.retana@omni.cr>
2020-11-02 21:31:30 -0600

    removed open-close functionality

--- components/AlbumCard/AlbumCard.jsx
+++ components/AlbumCard/AlbumCard.jsx
@@ -1,25 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import SongsList from '../SongsList/SongsList'
 
 import styles from './AlbumCard.module.scss'
 
 export default function AlbumCard ({ name, artwork, year, slug, songs, setEmbedCode }) {
-  const [isOpen, setIsOpen] = useState(false)
-
-  function onClickHandler () {
-    setIsOpen(!isOpen)
-  }
-
   return (
     <li className={[styles.AlbumCardListItem, styles[slug]].join(' ')}>
       <section className={styles.AlbumCard}>
         <img src={artwork} alt={`${name} artwork`} />
-        <div className={styles.TextContainer} onClick={onClickHandler}>
+        <div className={styles.TextContainer}>
           <h1 className={styles.AlbumTitle}>{name}</h1>
           <p className={styles.AlbumYear}>{year}</p>
         </div>
       </section>
-      {isOpen && (<SongsList songs={songs} slug={slug} setEmbedCode={setEmbedCode} />)}
+      <SongsList songs={songs} slug={slug} setEmbedCode={setEmbedCode} />
     </li>
   )
 }
