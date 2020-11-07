634eed6093d9611939f2127fb351aa20b1c8ad41
Mario Retana <mario.retana@omni.cr>
2020-11-06 22:33:28 -0600

    added font awsome icons

--- components/Song/Song.jsx
+++ components/Song/Song.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay } from '@fortawesome/free-solid-svg-icons'
 
 import styles from './Song.module.scss'
 
@@ -18,7 +20,9 @@ export default function Song ({ idSong, albumPosition, name, embedCode, isSelect
   return (
     <li className={styles.Song}>
       <p className={styles.SongName}>{`${albumPosition}. ${name}`}</p>
-      <button type='button' onClick={onSongPlay}>Play!</button>
+      <button type='button' className={styles.PlayButton} onClick={onSongPlay}>
+        <FontAwesomeIcon icon={faPlay} />
+      </button>
       <button type='button' className={styles.VoteButton} onClick={onToggleVote}>{isSelected ? 'X' : ''}</button>
     </li>
   )
