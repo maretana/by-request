6c0038bb2fc27bbf7d22dd6f07110108e036651b
Mario Retana <mario.retana@omni.cr>
2020-11-27 22:42:15 -0600

    added social links and submenu skeleton

--- components/Submenu/Submenu.jsx
+++ components/Submenu/Submenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import NavigationList from '../NavigationList'
 
@@ -14,6 +14,25 @@ export default function Submenu ({ isOpen, closeSubmenu }) {
       </button>
       <h1>Costa Rica by Request</h1>
       <NavigationList closeSubmenu={closeSubmenu} />
+      <section>
+        <h1>Follow us</h1>
+        <img width='100%' src='/chapter_logo_text.png' alt='Costa Rica Fade To Black logo' />
+        <nav>
+          <a href='https://www.facebook.com/CostaRicaFadeToBlack/' aria-label='Go to our Facebook page'>
+            <FontAwesomeIcon icon={faFacebook} />
+            <span>Facebook</span>
+          </a>
+          <a href='https://www.instagram.com/costaricafadetoblack/' aria-label='Go to our Instagram page'>
+            <FontAwesomeIcon icon={faInstagram} />
+            <span>Instagram</span>
+          </a>
+          <a href='https://twitter.com/CR_FadeToBlack' aria-label='Go to our Twitter page'>
+            <FontAwesomeIcon icon={faTwitter} />
+            <span>Twitter</span>
+          </a>
+        </nav>
+      </section>
+
     </div>
   )
 }
